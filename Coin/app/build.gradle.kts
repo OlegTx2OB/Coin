ec18f4f0d5d505255d022f7cc78a516b3d0c6a76
OlegTx2OB <olegtx2ob@gmail.com>
2024-03-12 14:20:36 +0300

    commit

--- Coin/app/build.gradle.kts
+++ Coin/app/build.gradle.kts
@@ -21,10 +21,10 @@ android {
         enable = true
     }
     defaultConfig {
-        applicationId = "com.example.coin"
+        applicationId = "com.tourball.coin"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
